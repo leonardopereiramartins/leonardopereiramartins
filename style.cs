2cec48b690c7908db68ea90e3a4a1ef528e8d57f
Leoo <105317366+leonardopereiramartins@users.noreply.github.com>
2023-06-28 08:52:51 -0300

    Site pronto

---
+++ style.cs
@@ -0,0 +1,470 @@
+/* http://meyerweb.com/eric/tools/css/reset/ 
+   v2.0 | 20110126
+   License: none (public domain)
+*/
+
+html, body, div, span, applet, object, iframe,
+h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+a, abbr, acronym, address, big, cite, code,
+del, dfn, em, img, ins, kbd, q, s, samp,
+small, strike, strong, sub, sup, tt, var,
+b, u, i, center,
+dl, dt, dd, ol, ul, li,
+fieldset, form, label, legend,
+table, caption, tbody, tfoot, thead, tr, th, td,
+article, aside, canvas, details, embed, 
+figure, figcaption, footer, header, hgroup, 
+menu, nav, output, ruby, section, summary,
+time, mark, audio, video {
+	margin: 0;
+	padding: 0;
+	border: 0;
+	font-size: 100%;
+	font: inherit;
+	vertical-align: baseline;
+}
+/* HTML5 display-role reset for older browsers */
+article, aside, details, figcaption, figure, 
+footer, header, hgroup, menu, nav, section {
+	display: block;
+}
+body {
+	line-height: 1;
+}
+ol, ul {
+	list-style: none;
+}
+blockquote, q {
+	quotes: none;
+}
+blockquote:before, blockquote:after,
+q:before, q:after {
+	content: '';
+	content: none;
+}
+table {
+	border-collapse: collapse;
+	border-spacing: 0;
+}
+
+
+
+
+body {
+  background: #006400;
+}
+
+
+#logo {
+  width: 30%;
+  box-shadow: 5px 5px 4px 3px #d8ecff;
+  
+}
+
+#logo2 {
+  width: 18%;
+}
+
+header {
+  background: #050f44;
+  padding: 20px 0;
+}
+
+.caixa {
+  position: relative;
+  width: 940px;
+  margin: 0 auto;
+  background;
+}
+
+nav {
+  position: absolute;
+  top:120px;
+  right:0;
+}
+
+nav li {
+  display: inline;
+  margin:0 0 0 15px ;
+}
+
+nav a {
+  text-transform: uppercase;
+  color: #FFFAFA;
+  font-weight: bold;
+  font-size: 26.5px;
+  text-decoration: none;
+  font-family: 'Press Start 2P', cursiva;
+}
+
+nav a:hover {
+  color: #FFD700;
+  text-decoration: underline;
+  transition: 0.5s all;
+}
+
+.jogos {
+  width: 940px;
+  margin: 0 auto;
+  padding: 50px 0;
+  font-family: MV Boli;
+  color: #0b153e;
+}
+
+.jogos h2 {
+  font-size: 25px;
+  font-weight: bold;
+  color: #0b153e;
+}
+
+.jogos li:hover {
+  border-color: #199aff;
+  color: #FFD700;
+  border: 4px solid;
+  transition: 0.7s all;
+  cursor: pointer;
+}
+
+
+
+.jogos li:hover h2 {
+  font-size: 32.4px;
+  color: #FFD700;
+  transition: 0.7s all;
+}
+
+
+.jogos li {
+  display:inline-block;
+  text-align: center;
+  width: 28%;
+  vertical-align: top;
+  margin: 0 1.5%;
+}
+
+#jogo1 {
+  width:80%;
+  border-radius: 15%;
+  box-shadow: 4px 2px 6px 5px;
+}
+#jogo2 {
+  width:80%;
+  border-radius: 15%;
+  box-shadow: 4px 2px 6px 5px;
+}
+#jogo3 {
+  width:80%;
+  border-radius: 15%;
+  box-shadow: 4px 2px 6px 5px;
+}
+#jogo4 {
+  width:88%;
+  border-radius: 15%;
+  box-shadow: 4px 2px 6px 5px;
+}
+#jogo5 {
+  width:88%;
+  border-radius: 15%;
+  box-shadow: 4px 2px 6px 5px;
+}
+#jogo6 {
+  width:68%;
+  border-radius: 15%;
+  box-shadow: 4px 2px 6px 5px;
+}
+#jogo7 {
+  width:80%;
+  border-radius: 15%;
+  box-shadow: 4px 2px 6px 5px;
+}
+#jogo8 {
+  width:84%;
+  border-radius: 15%;
+  box-shadow: 4px 2px 6px 5px;
+}
+#jogo9 {
+  width:80%;
+  border-radius: 15%;
+  box-shadow: 4px 2px 6px 5px;
+}
+#jogo10 {
+  width:80%;
+  border-radius: 15%;
+  box-shadow: 4px 2px 6px 5px;
+}
+#jogo11 {
+  width:66%;
+  border-radius: 15%;
+  box-shadow: 4px 2px 6px 5px;
+}
+#jogo12 {
+  width:81%;
+  border-radius: 15%;
+  box-shadow: 4px 2px 6px 5px;
+}
+#jogo13 {
+  width:90%;
+  border-radius: 15%;
+  box-shadow: 4px 2px 6px 5px;
+}
+#jogo14 {
+  width:83%;
+  border-radius: 15%;
+  box-shadow: 4px 2px 6px 5px;
+}
+#jogo15 {
+  width:83%;
+  border-radius: 15%;
+  box-shadow: 4px 2px 6px 5px;
+}
+#jogo16 {
+  width:83%;
+  border-radius: 15%;
+  box-shadow: 4px 2px 6px 5px;
+}
+#jogo17 {
+  width:72%;
+  border-radius: 15%;
+  box-shadow: 4px 2px 6px 5px;
+}
+#jogo18 {
+  width:82%;
+  border-radius: 15%;
+  box-shadow: 4px 2px 6px 5px;
+}
+#jogo19 {
+  width:80%;
+  border-radius: 15%;
+  box-shadow: 4px 2px 6px 5px;
+}
+#jogo20 {
+  width:80%;
+  border-radius: 15%;
+  box-shadow: 4px 2px 6px 5px;
+}
+#jogo21 {
+  width:80%;
+  border-radius: 15%;
+  box-shadow: 4px 2px 6px 5px;
+}
+#jogo22 {
+  width:90%;
+  border-radius: 15%;
+  box-shadow: 4px 2px 6px 5px;
+}
+#jogo23 {
+  width:88%;
+  border-radius: 15%;
+  box-shadow: 4px 2px 6px 5px;
+}
+#jogo24 {
+  width:82%;
+  margin: 9px;
+  border-radius: 15%;
+  box-shadow: 4px 2px 6px 5px;
+}
+
+#leozao {
+  width:74.5%;
+  margin: 9px;
+  border-radius: 15%;
+  box-shadow: 4px 2px 6px 5px;
+}
+
+#juliao {
+  width:74%;
+  margin: 9px;
+  border-radius: 15%;
+  box-shadow: 4px 2px 6px 5px;
+}
+
+.estrelas:hover {
+  color:#19c8ff;
+  font-size: 30.7px;
+  transition: 0.7s all;
+}
+
+
+.avaliacoes {
+  font-size: 18px;
+  margin:4px
+}
+
+.estrelas {
+  font-size: 28px;
+  font-weight: bold;
+  margin-top: 12px;
+  margin:5px
+}
+
+
+footer {
+  text-align: center;
+  background: #050f44;
+  padding: 40px;
+}
+
+.Copyright {
+   color: #ffffff;
+   font-size: 11px;
+   margin: 20px 0 0;
+}
+
+
+#logo12{
+  width:10%
+}
+h3 {
+  font-size:30px;
+  text-align: center;
+  color:#c0ded9;
+}
+
+.info {
+  color: #006400;
+}
+
+.info:hover {
+  color: #FFD700;
+  transition: 1.1s all;
+  cursor: help;
+}
+
+.xis:hover {
+ cursor:not-allowed ;
+}
+
+.julio {
+  position:relative;
+  left: 300px;
+  border: 4px outset #000000;
+  border-radius: 55px 8px 55px 8px;
+}
+
+.goleiro1 {
+  border: 6px double #790f7a;
+  border-radius: 55px 8px 55px 8px;
+}
+
+.goleiro2 {
+  border: 6px double #790f7a;
+  border-radius: 55px 8px 55px 8px;
+}
+
+.goleiro3 {
+  border: 6px double #790f7a;
+  border-radius: 55px 8px 55px 8px;
+}
+
+.lateral1 {
+  border: 6px double #ca0000;
+  border-radius: 55px 8px 55px 8px;
+}
+
+.lateral2 {
+  border: 6px double #ca0000;
+  border-radius: 55px 8px 55px 8px;
+}
+
+.lateral3 {
+  border: 6px double #ca0000;
+  border-radius: 55px 8px 55px 8px;
+}
+
+.lateral4 {
+  border: 6px double #ca0000;
+  border-radius: 55px 8px 55px 8px;
+}
+
+.zagueiro1 {
+  border: 6px double #000000;
+  border-radius: 55px 8px 55px 8px;
+}
+
+.zagueiro2 {
+  border: 6px double #000000;
+  border-radius: 55px 8px 55px 8px;
+}
+
+.zagueiro3 {
+  border: 6px double #000000;
+  border-radius: 55px 8px 55px 8px;
+}
+
+.zagueiro4 {
+  border: 6px double #000000;
+  border-radius: 55px 8px 55px 8px;
+}
+
+.meia1 {
+  border: 6px double #03ff08;
+  border-radius: 55px 8px 55px 8px;
+}
+
+.meia2 {
+  border: 6px double #03ff08;
+  border-radius: 55px 8px 55px 8px;
+}
+
+.meia3 {
+  border: 6px double #03ff08;
+  border-radius: 55px 8px 55px 8px;
+}
+
+.meia4 {
+  border: 6px double #03ff08;
+  border-radius: 55px 8px 55px 8px;
+}
+
+.meia5 {
+  border: 6px double #03ff08;
+  border-radius: 55px 8px 55px 8px;
+}
+
+.meia6 {
+  border: 6px double #03ff08;
+  border-radius: 55px 8px 55px 8px;
+}
+
+.atac1 {
+  border: 6px double #0000ff;
+  border-radius: 55px 8px 55px 8px;
+}
+
+.atac2 {
+  border: 6px double #0000ff;
+  border-radius: 55px 8px 55px 8px;
+}
+
+.atac3 {
+  border: 6px double #0000ff;
+  border-radius: 55px 8px 55px 8px;
+}
+
+.atac4 {
+  border: 6px double #0000ff;
+  border-radius: 55px 8px 55px 8px;
+}
+
+.atac5 {
+  border: 6px double #0000ff;
+  border-radius: 55px 8px 55px 8px;
+}
+
+.atac6 {
+  border: 6px double #0000ff;
+  border-radius: 55px 8px 55px 8px;
+}
+
+.tec {
+  font-size:10px;
+  border: 6px double #FFFFFF;
+  border-radius: 55px 8px 55px 8px;
+}
+
+
+
+.leo {
+  border: 4px outset #000000;
+  border-radius: 55px 8px 55px 8px;
+}
